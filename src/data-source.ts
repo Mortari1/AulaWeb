1ab57d7fce3422e40b85ccbc38ca9e7a078ca96c
HENRIQUE SILVA MORTARI <henrique.4105@aluno.pr.senac.br>
2023-09-04 19:29:20 -0300

    @Henrique #enviando a base da api

---
+++ src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+import { User } from "./entity/User"
+
+export const AppDataSource = new DataSource({
+        type: "mysql",
+        host: "127.0.0.1",
+        port: 3306,
+        username: "root",
+        password: "",
+        database: "web_project",
+        entities: [User],
+        synchronize: true,
+        logging:false
+        
+    })
